6d102dfa67a5e6e8bdee49bdcbe3c89503871181
ncksllvn <ncksllvn@gmail.com>
2021-09-10 00:03:23 -0400

    Add stub for sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,22 @@ module.exports = {
         'Times New Roman',
         'Times',
         'serif'
+      ],
+      sans: [
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
       ]
     },
     extend: {},
